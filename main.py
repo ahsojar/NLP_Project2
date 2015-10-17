f01eff75b3e2d7459f27cc3ea4f602a30be51b15
Alisha Sojar <ahsojar@dhcp-gs-1612.redrover.cornell.edu>
2015-10-17 14:08:49 -0400

    get's max sense probs

--- main.py
+++ main.py
@@ -1,9 +1,65 @@
 import xml.etree.ElementTree as etree
+import operator
 
-tree = etree.parse('training-data.data.xml')
-root = tree.getroot()
+def train():
+  tree = etree.parse('training-data.data.xml')
+  model = {}
+
+  for lexelt in tree.findall('lexelt'):
+    word_id = lexelt.get('item')
+    if word_id not in model:
+      model[word_id] = {}
+    for i in lexelt.findall('instance'):
+      # words before context
+      wordsbefore =  i.find('context').text
+      fourwordsbefore = wordsbefore.split()[-4:]
+
+      for w in fourwordsbefore:
+        for answer in i.findall('answer'):
+          senseID = answer.get('senseid')
+          if w in model[word_id]:
+            if senseID in model[word_id][w]:
+              model[word_id][w][senseID] += 1
+            else:
+              model[word_id][w][senseID] = 1
+          else:
+            model[word_id][w] = {}
+            model[word_id][w][senseID] = 1
+
+  #normalizes counts into probabilities
+  model_prob = {}
+  for word in model:
+    model_prob[word] ={}
+    for context in model[word]:
+      senses = model[word][context]
+      factor = 1.0/sum(senses.itervalues())
+      normalized = {k: v*factor for k, v in senses.iteritems()}
+      model_prob[word][context] = normalized
+  return model_prob
 
 
-for lexelt in tree.findall('lexelt'):
-  for i in lexelt.findall('instance'):
-    print i.get('id')
+
+def wsd(model_prob):
+  tree = etree.parse('test-data.data.xml')
+
+  for lexelt in tree.findall('lexelt'):
+    word_id = lexelt.get('item')
+    for i in lexelt.findall('instance'):
+      # words before context
+      wordsbefore =  i.find('context').text
+      fourwordsbefore = wordsbefore.split()[-4:]
+
+      sense_probs = {}
+      for w in fourwordsbefore:
+        if w in model_prob[word_id]:
+          senses = model_prob[word_id][w]
+          for s in senses:
+            if s in sense_probs:
+              sense_probs[s] *= model_prob[word_id][w][s]
+            else:
+              sense_probs[s] = model_prob[word_id][w][s]
+      print sense_probs
+
+
+trained = train()
+wsd(trained)
