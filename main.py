8cc6011e1e03a4a5b2d26de7f6b16ae0750d5135
Alisha Sojar <ahsojar@dhcp-rhodes-609.redrover.cornell.edu>
2015-10-19 11:24:44 -0400

    alisha

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import xml.etree.ElementTree as etree
 import operator
 from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+
 
 def train():
-
-  soup = BeautifulSoup(open("training-data.data.xml"))
+  tree = BeautifulSoup(open("training-data.data.xml"))
   model = {}
+  prior_prob = {}
 
-  for lexelt in soup.find_all('lexelt'):
+  for lexelt in tree.find_all('lexelt'):
     word_id = lexelt.get('item')
+    prior_prob[word_id] = {}
     if word_id not in model:
       model[word_id] = {}
     for i in lexelt.find_all('instance'):
       # words before context
       wordsbefore =  i.find('context').contents[0]
-      fourwordsbefore = wordsbefore.split()[-4:]
-
       wordsafter =  i.find('context').contents[2]
-      fourwordsafter = wordsafter.split()[-4:]
 
-      contextwords = fourwordsbefore + fourwordsafter
+      contextwords = get_context_words(wordsbefore,wordsafter,4, True)
 
       for w in contextwords:
         for answer in i.find_all('answer'):
           senseID = answer.get('senseid')
+          if senseID not in prior_prob[word_id]:
+            prior_prob[word_id][senseID] = 1
+          else: 
+            prior_prob[word_id][senseID] += 1
+
           if w in model[word_id]:
             if senseID in model[word_id][w]:
               model[word_id][w][senseID] += 1
@@ -42,23 +48,33 @@ def train():
       factor = 1.0/sum(senses.itervalues())
       normalized = {k: v*factor for k, v in senses.iteritems()}
       model_prob[word][context] = normalized
-  return model_prob
+  
+  #normalize prior probabilities
+  for word in prior_prob:
+      senses = prior_prob[word]
+      factor = 1.0/sum(senses.itervalues())
+      normalized = {k: v*factor for k, v in senses.iteritems()}
+      prior_prob[word] = normalized
+
+  return model_prob, prior_prob
 
 
+def wsd(model_prob, prior_prob):
+  tree = BeautifulSoup(open("test-data.data.xml"))
 
-def wsd(model_prob):
-  tree = etree.parse('test-data.data.xml')
-
-  for lexelt in tree.findall('lexelt'):
+  for lexelt in tree.find_all('lexelt'):
     word_id = lexelt.get('item')
-    for i in lexelt.findall('instance'):
+    for i in lexelt.find_all('instance'):
       # words before context
       instance_id = i.get('id')
-      wordsbefore =  i.find('context').text
-      fourwordsbefore = wordsbefore.split()[-4:]
 
+      wordsbefore =  i.find('context').contents[0]
+      wordsafter =  i.find('context').contents[2]
+
+      contextwords = get_context_words(wordsbefore,wordsafter,4, True)
       sense_probs = {}
-      for w in fourwordsbefore:
+
+      for w in contextwords:
         if w in model_prob[word_id]:
           senses = model_prob[word_id][w]
           for s in senses:
@@ -66,8 +82,26 @@ def wsd(model_prob):
               sense_probs[s] *= model_prob[word_id][w][s]
             else:
               sense_probs[s] = model_prob[word_id][w][s]
+      #multiply sense probs by prior prob
+      for s in sense_probs:
+        if s in prior_prob:
+          sense_probs[s]*= prior_prob[s]
       print sense_probs
 
+# Returns context words given text before, after, a window size, 
+# and a boolean indicating whether to remove stopwords
+def get_context_words(contextbefore, contextafter, window, remove_stopwords):
+  if remove_stopwords:
+    stopwords = nltk.corpus.stopwords.words('english')
+    words_before = [w for w in contextbefore.split() if w.lower() not in stopwords]
+    words_after = [w for w in contextafter.split() if w.lower() not in stopwords]
+    contextwords = words_before[-window:] + words_after[-window:]
+  else:
+    contextwords = contextbefore.split()[-window:] + contextafter.split()[-window:]
+  return contextwords
 
 trained = train()
-wsd(trained)
+wsd(trained[0], trained[1])
+
+
+
